180db30e8d74d0fc842486c3b38e2dbd3c8c3b84
RaduMilici <radu.milici@3pillarglobal.com>
2017-07-04 18:37:12 +0300

    108. Validating props

---
+++ 08. Communicating between Components/104. Communication Problems/src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>user name: {{reverseName}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            'name': [String, Array]
+        },
+        computed: {
+            reverseName() {
+                return this.name.split('').reverse().join('');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
